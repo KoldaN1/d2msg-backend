1d21d4e606f88e8f98589d8a6f71a26983bb23ad
reCAPTCHA <53586412+KoldaN1@users.noreply.github.com>
2025-07-24 10:20:20 +0200

    Minor fixes

--- auth-service/src/config/index.ts
+++ auth-service/src/config/index.ts
@@ -4,8 +4,9 @@ dotenv.config();
 export const config = {
   host: process.env.HOST || "localhost",
   port: parseInt(process.env.PORT || "3000", 10),
-  jwtSecret: process.env.JWT_SECRET || "default_secret",
+  jwtSecret: process.env.JWT_SECRET,
   isDev: process.env.NODE_ENV !== "production",
   rabbitUrl: process.env.RABBITMQ_URL || "amqp://localhost",
-  databaseUrl: process.env.DATABASE_URL,
+  databaseUrl: process.env.MONGO_URL,
+  redisUrl: process.env.REDIS_URL || "redis://redis:6379",
 };
